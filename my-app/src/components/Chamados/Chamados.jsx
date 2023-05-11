2d39602148450126bf05e77d15ec466098e6adac
Lucca <luccalanzellotti@gmail.com>
2023-05-11 10:52:55 -0300

    Update

--- my-app/src/components/Chamados/Chamados.jsx
+++ my-app/src/components/Chamados/Chamados.jsx
@@ -2,21 +2,20 @@
 import Menu from "../Menu/Menu";
 function Chamados(){
     return(
-        <div> 
-       
-    <h1>Abertura de Chamados</h1>
-    <div style={{display: 'flex'}}>
-    <p>Título</p>
-    <input type="name" /><input/>
-    <p>Usuário</p>
-    <input type="name" /><input/>
-    </div>
-    < div style={{display: 'flex'}}>
-    <p>Pessoa Responsável</p>
-    <input type="name" /><input/>
-    <p>Departamento enviado</p>    <p>Tecnologia</p>
+      <div> 
+        <h1>Abertura de Chamados</h1>
+        <div style={{display: 'flex'}}>
+          <p>Título</p>
+          <input type="name" /><input/>
+          <p>Usuário</p>
+          <input type="name" /><input/>
+        </div>
+        < div style={{display: 'flex'}}>
+          <p>Pessoa Responsável</p>
+          <input type="name" /><input/>
+          <p>Departamento enviado</p>    <p>Tecnologia</p>
+        </div>
       </div>
-    </div>
     
     )
 }
